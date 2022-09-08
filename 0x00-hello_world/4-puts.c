469134fbaf203c7060af1fa0a88dcea12d999c52
Akinsanya <somatil024@gmail.com>
2022-09-08 13:36:19 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * Return: 1 if successful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
